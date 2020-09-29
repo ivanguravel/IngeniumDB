62c606cefde416e9d98f8d22261f36549f397a98
Ivan Zhuravel <ivanguravel@gmail.com>
2020-09-30 00:07:03 +0300

    decompress function

--- bzip-indexer/src/test/java/org/ivzh/bzip/indexer/stream/ExtendedBzipInputStreamTest.java
+++ bzip-indexer/src/test/java/org/ivzh/bzip/indexer/stream/ExtendedBzipInputStreamTest.java
@@ -1,10 +1,12 @@
 package org.ivzh.bzip.indexer.stream;
 
 import org.ivzh.bzip.indexer.dto.Block;
+import org.ivzh.bzip.indexer.util.CompressDecompressUtil;
 import org.ivzh.bzip.indexer.util.IndexUtil;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -17,5 +19,15 @@ public class ExtendedBzipInputStreamTest {
         IndexUtil.string2file(json, "test.json");
         Assert.assertNotNull(blocks);
         Assert.assertEquals(10, blocks.size());
+    }
+
+    @Test
+    public void testDecompressor() throws IOException {
+        String from = "src/main/java/org/ivzh/bzip/indexer/stream/8381.tar.bz2";
+        String to = "src/main/java/org/ivzh/bzip/indexer/stream/8381.tar";
+        CompressDecompressUtil.decompress(from, to);
+        File outputFile = new File (to);
+        Assert.assertTrue(outputFile.exists());
+        outputFile.delete();
     }
 }
